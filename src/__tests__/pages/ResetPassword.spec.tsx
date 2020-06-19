0b9372bfe5ad782bf73647fd6a0403b0595b0306
Lucas de Castro <castro.lucas@engenharia.ufjf.br>
2020-06-18 21:54:44 -0300

    finalizando testes em mais páginas

---
+++ src/__tests__/pages/ResetPassword.spec.tsx
@@ -0,0 +1,156 @@
+import React from 'react';
+import MockAdapter from 'axios-mock-adapter';
+import { render, fireEvent, wait } from '@testing-library/react';
+import ResetPassword from '../../pages/ResetPassword';
+import api from '../../services/api';
+
+const apiMock = new MockAdapter(api);
+const mockedHistoryPush = jest.fn();
+const mockedSearch = jest.fn();
+const mockedAddToast = jest.fn();
+
+jest.mock('react-router-dom', () => {
+  return {
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
+    useLocation: () => ({
+      search: {
+        replace: mockedSearch,
+      },
+    }),
+  };
+});
+
+jest.mock('../../hooks/toast', () => {
+  return {
+    useToast: () => ({
+      addToast: mockedAddToast,
+    }),
+  };
+});
+
+describe('ResetPassword Page', () => {
+  beforeEach(() => {
+    mockedHistoryPush.mockClear();
+    mockedSearch.mockClear();
+    mockedAddToast.mockClear();
+  });
+
+  it('should be able do reset the password', async () => {
+    mockedSearch.mockImplementation(() => {
+      return 'token-123';
+    });
+
+    const { getByPlaceholderText, getByText } = render(<ResetPassword />);
+
+    const passwordField = getByPlaceholderText('Nova senha');
+    const passwordConfirmationField = getByPlaceholderText(
+      'Confirme sua senha',
+    );
+    const buttonElement = getByText('Alterar senha');
+
+    fireEvent.change(passwordField, { target: { value: '123123' } });
+    fireEvent.change(passwordConfirmationField, {
+      target: { value: '123123' },
+    });
+
+    fireEvent.click(buttonElement);
+
+    apiMock.onPost('password/reset').reply(200);
+
+    await wait(() => {
+      expect(mockedHistoryPush).toHaveBeenCalledWith('/');
+    });
+  });
+
+  it('should not be able do reset the password with invalid password confirmation', async () => {
+    mockedSearch.mockImplementation(() => {
+      return 'token-123';
+    });
+
+    const { getByPlaceholderText, getByText } = render(<ResetPassword />);
+
+    const passwordField = getByPlaceholderText('Nova senha');
+    const passwordConfirmationField = getByPlaceholderText(
+      'Confirme sua senha',
+    );
+    const buttonElement = getByText('Alterar senha');
+
+    fireEvent.change(passwordField, { target: { value: '123123' } });
+    fireEvent.change(passwordConfirmationField, {
+      target: { value: 'wrong-confirmation' },
+    });
+
+    fireEvent.click(buttonElement);
+
+    apiMock.onPost('password/reset').reply(200);
+
+    await wait(() => {
+      expect(mockedHistoryPush).not.toHaveBeenCalled();
+    });
+  });
+
+  it('should not be able do reset the password with invalid token', async () => {
+    mockedSearch.mockImplementation(() => {
+      return undefined;
+    });
+
+    const { getByPlaceholderText, getByText } = render(<ResetPassword />);
+
+    const passwordField = getByPlaceholderText('Nova senha');
+    const passwordConfirmationField = getByPlaceholderText(
+      'Confirme sua senha',
+    );
+    const buttonElement = getByText('Alterar senha');
+
+    fireEvent.change(passwordField, { target: { value: '123123' } });
+    fireEvent.change(passwordConfirmationField, {
+      target: { value: '123123' },
+    });
+
+    fireEvent.click(buttonElement);
+
+    apiMock.onPost('password/reset').reply(200);
+
+    await wait(() => {
+      expect(mockedHistoryPush).not.toHaveBeenCalled();
+      expect(mockedAddToast).toHaveBeenCalledWith(
+        expect.objectContaining({
+          type: 'error',
+        }),
+      );
+    });
+  });
+
+  it('should display an error if reset password fails', async () => {
+    mockedSearch.mockImplementation(() => {
+      return 'token-123';
+    });
+
+    const { getByPlaceholderText, getByText } = render(<ResetPassword />);
+
+    const passwordField = getByPlaceholderText('Nova senha');
+    const passwordConfirmationField = getByPlaceholderText(
+      'Confirme sua senha',
+    );
+    const buttonElement = getByText('Alterar senha');
+
+    fireEvent.change(passwordField, { target: { value: '123123' } });
+    fireEvent.change(passwordConfirmationField, {
+      target: { value: '123123' },
+    });
+
+    fireEvent.click(buttonElement);
+
+    apiMock.onPost('password/reset').networkError();
+
+    await wait(() => {
+      expect(mockedAddToast).toHaveBeenCalledWith(
+        expect.objectContaining({
+          type: 'error',
+        }),
+      );
+    });
+  });
+});
